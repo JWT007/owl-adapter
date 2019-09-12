f5b294d9c49e580cbc8ca76d04910829127a0620
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2019-09-12 14:59:21 -0700

    more complete tests

--- io.opencaesar.owl.adapter/io.opencaesar.oml2owl/src/test/java/io/opencaesar/oml2owl/utils/TestComplement.java
+++ io.opencaesar.owl.adapter/io.opencaesar.oml2owl/src/test/java/io/opencaesar/oml2owl/utils/TestComplement.java
@@ -23,10 +23,15 @@ public class TestComplement {
 	private Singleton sa1;
 	private Singleton sa2;
 	private Singleton sb;
+	private Singleton sc;
 	
 	private Complement ca1;
 	private Complement ca2;
 	private Complement cb;
+	private Complement cc;
+	
+	private Empty empty;
+	private Universal universal;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -41,9 +46,13 @@ public class TestComplement {
 		sa1 = new Singleton("a");
 		sa2 = new Singleton("a");
 		sb = new Singleton("b");
+		sc = new Singleton("c");
 		ca1 = new Complement(sa1);
 		ca2 = new Complement(sa2);
 		cb = new Complement(sb);
+		cc = new Complement(sc);
+		empty = new Empty();
+		universal = new Universal();
 	}
 
 	@After
@@ -79,6 +88,9 @@ public class TestComplement {
 		assertEquals(sa1, ca1.complement());
 		assertEquals(sa2, ca2.complement());
 		assertEquals(sb, cb.complement());
+		// Theorem 17
+		assertEquals(universal, empty.complement());
+		assertEquals(empty, universal.complement());
 	}
 
 	@Test
@@ -103,6 +115,17 @@ public class TestComplement {
 		Difference bma = new Difference(cb, ca1);
 		assertEquals(amb, ca1.difference(cb));
 		assertEquals(bma, cb.difference(ca1));
+		// Theorem 8
+		Complement sl[] = {cb, cc};
+		HashSet<ClassExpression> s = new HashSet<ClassExpression>(Arrays.asList(sl));
+		Union u = new Union(s);
+		assertEquals(ca1.difference(u), ca1.difference(cb).difference(cc));
+		// Theorem 11
+		assertEquals(ca1, ca1.difference(empty));
+		// Theorem 13
+		assertEquals(empty, ca1.difference(ca1));
+		// Theorem 16
+		assertEquals(empty, ca1.difference(universal));
 	}
 
 	@Test
@@ -112,6 +135,13 @@ public class TestComplement {
 		Intersection i = new Intersection(s);
 		assertEquals(i, ca1.intersection(cb));
 		assertEquals(i, cb.intersection(ca1));
+		// Theorem 2
+		assertEquals(ca1, ca1.intersection(ca1));
+		// Theorem 3
+		assertEquals(ca1.intersection(cb), cb.intersection(ca1));
+		// Theorem 4
+		assertEquals((ca1.intersection(cb)).intersection(cc), (ca1.intersection(cb)).intersection(cc));
+		// Theorem 14
 	}
 
 	@Test
@@ -121,6 +151,13 @@ public class TestComplement {
 		Union u = new Union(s);
 		assertEquals(u, ca1.union(cb));
 		assertEquals(u, cb.union(ca1));
+		// Theorem 5
+		assertEquals(ca1, ca1.union(ca1));
+		// Theorem 6
+		assertEquals(ca1.union(cb), cb.union(ca1));
+		// Theorem 7
+		assertEquals((ca1.union(cb)).union(cc), (ca1.union(cb)).union(cc));		
+		// Theorem 15
 	}
 
 }
